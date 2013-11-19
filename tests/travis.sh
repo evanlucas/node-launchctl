d12401c5ef2c8df5121745392214e966007a6aad
Evan Lucas <evan.lucas@hattiesburgclinic.com>
2013-11-19 14:21:58 -0600

    Download the latest node for travis

--- tests/travis.sh
+++ tests/travis.sh
@@ -4,7 +4,7 @@ NODE=$(which node)
 
 if [[ "$?" != "0" ]]; then
   echo "Fetching node.js"
-  wget http://nodejs.org/dist/v0.10.21/node-v0.10.21.pkg -O node.pkg
+  wget http://nodejs.org/dist/v0.10.22/node-v0.10.22.pkg -O node.pkg
   sudo installer -pkg node.pkg -target /
   hash -r
 fi
