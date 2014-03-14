d64ec5a7c5db32dabb043794f2a7abf57c7bb544
Evan Lucas <evanlucas@me.com>
2014-03-13 23:53:02 -0500

    show some errors and make n be local

--- test/pre_travis.sh
+++ test/pre_travis.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ELLOG_HEADING="launchctl"
+source "$DIR"/ellog.bash
+
 N=$(which n)
 if [[ $? != "0" ]]; then
   ellog_info "n" "not installed"
   ellog_info "n" "installing..."
   git clone https://github.com/visionmedia/n
-  cd n
-  make install
-  cd ..
+  cp n/bin/n .
+else
+  ellog_info "n" "already installed"
 fi
--- test/travis.sh
+++ test/travis.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export N_PREFIX="$DIR/versions"
 ELLOG_HEADING="launchctl"
 source "$DIR"/ellog.bash
-
-N=$(which n)
-if [[ $? != "0" ]]; then
-  ellog_info "n" "not installed"
-  ellog_info "n" "installing..."
-  git clone https://github.com/visionmedia/n
-  cd n
-  make install
-  cd ..
-fi
 
 VERSIONS=("0.8.26" "0.10.20" "0.10.21" "0.10.22" "0.10.23" "0.10.24" "0.10.25" "0.10.26" "0.11.6" "0.11.7" "0.11.8" "0.11.9" "0.11.10")
 
@@ -19,7 +10,7 @@ VERSIONS=("0.8.26" "0.10.20" "0.10.21" "0.10.22" "0.10.23" "0.10.24" "0.10.25" "
 # 0.11.12 messed up MakeCallback so async addon functions fail
 
 for vers in ${VERSIONS[*]}; do
-  n "$vers"
+  ./n "$vers"
   ellog_info "$vers" "installing dependencies"
   npm install --silent >/dev/null
   if [[ $? != "0" ]]; then
