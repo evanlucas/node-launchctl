66f46ea2606ac17fa2b4ad24521b50d2d05359f1
Evan Lucas <evanlucas@me.com>
2014-03-17 04:59:51 -0500

    Looks like installing the package requires root
    try installing for tarball

--- scripts/pre_travis.sh
+++ scripts/pre_travis.sh
@@ -5,8 +5,11 @@ ELLOG_HEADING="launchctl"
 source "$DIR"/ellog.bash
 
 ellog_info "install" "stable node"
-wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.pkg -O node.pkg
-sudo installer -pkg node.pkg -target /
+mkdir -p node
+cd node
+curl -L# http://nodejs.org/dist/v0.10.26/node-v0.10.26-darwin-x64.tar.gz | tar -zx --strip 1
+cp -fR * /usr/local/
+cd ..
 hash -r
 
 ellog_info "install" "n" "global"
